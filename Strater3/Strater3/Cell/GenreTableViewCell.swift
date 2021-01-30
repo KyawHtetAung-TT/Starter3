ddc4b1b3e13ccc940be481f0555a92f42c4dc228
Kyaw Htet Aung <93kyawhtetaung@gmaill.com>
2021-01-30 14:44:23 +0630

    P 11 Genre Movie

--- Strater3/Strater3/Cell/GenreTableViewCell.swift
+++ Strater3/Strater3/Cell/GenreTableViewCell.swift
@@ -20,8 +20,12 @@ class GenreTableViewCell: UITableViewCell {
         
         collectionViewGenre.dataSource = self
         collectionViewGenre.delegate = self
-        
         collectionViewGenre.register(UINib(nibName: String(describing: GenreCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: String(describing: GenreCollectionViewCell.self))
+        
+        
+        collectionViewMovie.dataSource = self
+        collectionViewMovie.delegate = self
+        collectionViewMovie.register(UINib(nibName: String(describing: PopularFilmCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: String(describing: PopularFilmCollectionViewCell.self))
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -34,10 +38,21 @@ class GenreTableViewCell: UITableViewCell {
 
 extension GenreTableViewCell : UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        if collectionView == collectionViewMovie{
+            return 10
+        }
         return genreList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == collectionViewMovie{
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: PopularFilmCollectionViewCell.self), for: indexPath) as? PopularFilmCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            return cell
+            
+        }else{
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: GenreCollectionViewCell.self), for: indexPath) as? GenreCollectionViewCell else {
             return UICollectionViewCell()
         }
@@ -56,8 +71,16 @@ extension GenreTableViewCell : UICollectionViewDataSource,UICollectionViewDelega
         }
         
         return cell
+        }
+            
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if collectionView == collectionViewMovie{
+         
+            return CGSize(width: collectionView.frame.width/3, height: 240)
+        }
+        
         return CGSize(width: widthOfString(text: genreList[indexPath.row].name,font: UIFont(name: "Geeza Pro Regular", size: 14) ?? UIFont.systemFont(ofSize: 14))+40, height: 40)
     }
     
