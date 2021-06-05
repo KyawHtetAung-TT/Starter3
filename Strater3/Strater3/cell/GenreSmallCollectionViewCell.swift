3e0afca0b6916dc5fcc10389adfa6090c3551704
Ryan Willson <93kyawhtetaung@gmail.com>
2021-06-06 01:05:05 +0630

    p 23 Actors

---
+++ Strater3/Strater3/cell/GenreSmallCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GenreSmallollectionViewCell.swift
+//  Strater3
+//
+//  Created by Ryan Willson on 6/5/21.
+//
+
+import UIKit
+
+class GenreSmallCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var lblGenre: UILabel!
+    
+    var GenreName = ["GMG","gjdj","sjf"]
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        
+        
+    }
+
+}
--- Strater3/Strater3/viewController/MovieDetailViewController.swift
+++ Strater3/Strater3/viewController/MovieDetailViewController.swift
@@ -9,16 +9,40 @@ import UIKit
 
 class MovieDetailViewController: UIViewController {
 
+    
     @IBOutlet weak var ivBack: UIImageView!
+    @IBOutlet weak var btnRateMovie: UIButton!
+    @IBOutlet weak var collectionViewGenre: UICollectionView!
+    @IBOutlet weak var collectionViewActor: UICollectionView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         initGestureRecognizerBack()
-        
+        btnBorderColor()
+        registerCollectionView()
+        GenreSamllregisterCollectionView()
+//        btnRateMovie.isUserInteractionEnabled = false
+
         // Do any additional setup after loading the view.
     }
     
+    private func registerCollectionView(){
+        
+        collectionViewActor.dataSource = self
+        collectionViewActor.delegate = self
+        collectionViewActor.registerForCell(identifier: BestActorCollectionViewCell.identifier)
+        
+    }
+    private func GenreSamllregisterCollectionView(){
+        
+        collectionViewGenre.dataSource = self
+        collectionViewGenre.delegate = self
+        collectionViewGenre.registerForCell(identifier: GenreSmallCollectionViewCell.identifier)
+        
+    }
     
     private func initGestureRecognizerBack(){
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(onTapButton))
@@ -33,4 +57,46 @@ class MovieDetailViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
         
     }
+    
+    func btnBorderColor(){
+        btnRateMovie.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        btnRateMovie.layer.borderWidth = 1
+        btnRateMovie.layer.cornerRadius = 20
+    }
+
 }
+extension MovieDetailViewController : UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == collectionViewActor{
+        return 5
+        }
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == collectionViewActor{
+            
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: BestActorCollectionViewCell.self), for: indexPath) as? BestActorCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        
+//        let cell = collectionView.dequeCell(identifier: BestActorCollectionViewCell.identifier, indexPath:  indexPath)
+        
+        return cell
+        }
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: GenreSmallCollectionViewCell.self), for: indexPath) as? GenreSmallCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+       
+            return cell
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/2, height: CGFloat(220))
+    }
+    
+    
+    
+}
+
